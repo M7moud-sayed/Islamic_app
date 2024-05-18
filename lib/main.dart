a20f38f9ff04732bc29e4bd740b884b72ae41674
M7moud-sayed <ma7mooudsayed@gmail.com>
2024-05-18 10:30:32 +0300

    initial islamic app

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:islamic_app/ui/home/home_screen.dart';
+import 'package:islamic_app/ui/quran_details/quran_details_screen.dart';
+import 'package:islamic_app/ui/splash/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Islamic App',
+      theme: ThemeData(
+        primaryColor: Color(0xFFB7935F),
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: "El Messiri",
+            )),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFFB7935F),
+          primary: Color(0xFFB7935F),
+          secondary: Color(0xFFB7935F).withOpacity(0.57),
+          onPrimary: Colors.white,
+          onSecondary: Colors.black,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Color(0xFFB7935F),
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          selectedIconTheme: IconThemeData(size: 32),
+          unselectedIconTheme: IconThemeData(size: 25),
+        ),
+        dividerTheme: const DividerThemeData(
+          color: Color(0xFFB7935F),
+          thickness: 1.5,
+        ),
+        useMaterial3: true,
+      ),
+      initialRoute: SplashScreen.routeName,
+      routes: {
+        SplashScreen.routeName: (_) => const SplashScreen(),
+        HomeScreen.routeName: (_) => const HomeScreen(),
+        QuranDetailsScreen.routeName: (_) => QuranDetailsScreen(),
+      },
+    );
+  }
+}
---
+++ lib/ui/home/hadeth_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class HadethWidget extends StatelessWidget {
+  const HadethWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
