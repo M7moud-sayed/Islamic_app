ed1d1dae917ed09e3353e60ab6ef1ee2590bd147
M7moud-sayed <ma7mooudsayed@gmail.com>
2024-05-23 09:51:22 +0300

    finished radio widget

--- lib/ui/home/quran_title_widget.dart
+++ lib/ui/home/quran_title_widget.dart
@@ -5,32 +5,36 @@ class QuranTitleWidget extends StatelessWidget {
   String title;
   String versesNumber;
   int index;
-  QuranTitleWidget({super.key,required this.title,required this.versesNumber,required this.index});
+
+  QuranTitleWidget(
+      {super.key,
+      required this.title,
+      required this.versesNumber,
+      required this.index});
 
   @override
   Widget build(BuildContext context) {
-
     return InkWell(
-      onTap: (){
-        Navigator.pushNamed(context, QuranDetailsScreen.routeName,arguments: QuranDetailsArgs(title, index));
+      onTap: () {
+        Navigator.pushNamed(context, QuranDetailsScreen.routeName,
+            arguments: QuranDetailsArgs(title, index));
       },
       child: Row(
         children: [
           Expanded(
-
-
-              child: Text(title,style: TextStyle(fontSize: 26),textAlign: TextAlign.center),),
-
-
+            child: Text(title,
+                style: TextStyle(fontSize: 26), textAlign: TextAlign.center),
+          ),
           Container(
             width: 1.5,
             height: 35,
             color: Color(0xFFB7935F),
           ),
-          Expanded(child: Text(versesNumber.toString(),style: TextStyle(fontSize: 26),textAlign: TextAlign.center)),
-
-  ],
-    ),
+          Expanded(
+              child: Text(versesNumber.toString(),
+                  style: TextStyle(fontSize: 26), textAlign: TextAlign.center)),
+        ],
+      ),
     );
   }
 }
